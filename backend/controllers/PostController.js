7cccc044bf200154d23f6335be4feb072d00cc50
Victor German Diaz <vitorx06@hotmail.com>
2023-11-30 00:42:34 -0300

    endPoint y logica para editar post terminada

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -4,14 +4,13 @@ const PostController = {}
 
 //CREAR POST
 PostController.createPost = async (req,res) => {
-    
-    try{
-        /**
-          * Recibimos los datos del formularios de creación de post del front,
-          * el author viene de la respuesta del middleware de token en req
-        */
-        const { title, description, imageURL } = req.body        
+    /**
+     * Recibimos los datos del formularios de creación de post del front,
+     * el author viene de la respuesta del middleware de token en req
+     */
+    const { title, description, imageURL } = req.body
 
+    try {
         //Creamos el post a insertar en la bd con el modelo
         const newPost = new Post({
             title,
@@ -23,15 +22,46 @@ PostController.createPost = async (req,res) => {
         const createPost = await newPost.save() //método mongoose para guardar en su db
 
         //Si se inserto exitosamente en db
-        res.status(201).json({ok: true, mensaje: 'Publicación creada exitosamente'})
-    }catch(err){
+        res.status(201).json({
+            ok: true,
+            message: 'Publicación creada exitosamente',
+        })
+    } catch (err) {
         console.log(err)
         res.status(400).json({
-            ok     : false,
-            mensaje: 'Ocurrió un error al intentar crear la publicación',
-            err
-        });
+            ok: false,
+            message: 'Ocurrió un error al intentar crear la publicación',
+            err,
+        })
     }
 }
+
+//EDITAR POST
+PostController.editPost = async (req,res) => {
+    // Recibimos los datos del formularios de edición de post del front,
+    const { id, title, description, imageURL } = req.body
+
+    try {
+        //Encontramos por id el post en la BD
+        const foundPost = await Post.findById(id);
+        
+        //Comparamos que el autor sea el mismo del que recibimos de la validación de token
+        if (foundPost.author.toString() !== req.uid)
+            return res.status(403).json({ ok: false, message: 'Debe ser el autor del post para editarlo' })
+
+        await Post.findByIdAndUpdate(id, { title, description, imageURL }) //Si el autor es el mismo editamos el post
+
+        res.status(200).json({ ok: true, message: 'Poste editado exitosamente' })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            message: 'Ocurrió un error al intentar editar la publicación',
+            err
+        })
+    }
+}
+
+
 
 module.exports = PostController
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,17 +1,19 @@
 /**
  * Endpoints de gestión de post, llaman a sus respectivos controladores desde ./controllers
+ * Usaremos el middleware para validar el token en cada ruta esto nos da acceso a la info del mismo por request
+ * pudiendo fijar asi valores como el uid del usuario
  */
-
 
 const postRouter = require('express').Router()  
 const PostController = require('../controllers/PostController')
 const { validarJWT } = require('../middleware/validarJWT')
 
-//Usaremos como middleware el token esto protege a todas las ruts si el token es incorrecto
 
 
 //Endpoint crear post
 postRouter.post('/create', validarJWT, PostController.createPost) // ./api/post/create method POST
+//Endpoint editar post
+postRouter.put('/edit', validarJWT, PostController.editPost)      // ./api/post/edit method PUT
 
 
 module.exports = postRouter
